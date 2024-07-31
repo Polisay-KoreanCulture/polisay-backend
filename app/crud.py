2ec897acafd0f580043a0ffc8a964836243d2c68
num22yh <num22yh@gmail.com>
2024-07-31 11:51:54 +0900

    이름 & 생년월일 api 추가

--- app/crud.py
+++ app/crud.py
@@ -4,3 +4,7 @@ from app import models, schemas
 
 def get_politicians(db: Session, skip: int = 0, limit: int = 500):
     return db.query(models.Politician).offset(skip).limit(limit).all()
+
+
+def get_politicians_by_name_and_birth_date(db: Session, name: str, birth_date: str):
+    return db.query(models.Politician).filter(models.Politician.name == name, models.Politician.birth_date == birth_date).all()
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app import models, schemas, crud
 from app.database import engine, get_db
+from typing import List
 
 models.Base.metadata.create_all(bind=engine) #테이블 생성
 
@@ -16,4 +17,9 @@ def read_root():
 @app.get("/politicians/", response_model=list[schemas.Politician])
 def read_politicians(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     politicians = crud.get_politicians(db, skip=skip, limit=limit)
+    return politicians
+
+@app.get("/politicians/search/", response_model=List[schemas.Politician])
+def search_politicians(name: str, birth_date: str, db: Session = Depends(get_db)):
+    politicians = crud.get_politicians_by_name_and_birth_date(db, name=name, birth_date=birth_date)
     return politicians
